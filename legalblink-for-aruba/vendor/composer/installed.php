282f98b2ba52182542a8f144e613c7faadbb296c
Sean Murphy <sean@iamseanmurphy.com>
2026-05-13 11:52:45 +0000

    1.1.4

--- legalblink-for-aruba/vendor/composer/installed.php
+++ legalblink-for-aruba/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'legalblink/legalblink-for-aruba',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b8bdd1de55737f195a7d44d5f59699feabf523fd',
+        'reference' => '93060fa74a5be6190360f9646b78da0fb82785c9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'legalblink/legalblink-for-aruba' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b8bdd1de55737f195a7d44d5f59699feabf523fd',
+            'reference' => '93060fa74a5be6190360f9646b78da0fb82785c9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
